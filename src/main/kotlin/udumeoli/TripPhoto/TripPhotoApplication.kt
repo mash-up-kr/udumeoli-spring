961124cb61b4c29043f2566bf68ce546c692889e
junhyukkkk <cjunk0304@gmail.com>
2026-05-24 17:00:48 +0900

    chore: initial commit from start.spring.io

---
+++ src/main/kotlin/udumeoli/TripPhoto/TripPhotoApplication.kt
@@ -0,0 +1,11 @@
+package udumeoli.TripPhoto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TripPhotoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TripPhotoApplication>(*args)
+}
---
+++ src/test/kotlin/udumeoli/TripPhoto/TripPhotoApplicationTests.kt
@@ -0,0 +1,13 @@
+package udumeoli.TripPhoto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TripPhotoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

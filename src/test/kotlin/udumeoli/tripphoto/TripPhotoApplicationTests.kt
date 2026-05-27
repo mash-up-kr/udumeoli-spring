3c326c0de6e9631bb239ba72dc7598c47f32ed93
junhyukkkk <cjunk0304@gmail.com>
2026-05-28 02:25:56 +0900

    build: ktlint/detekt 통과를 위한 코드/설정 보정
    - 패키지명 udumeoli.TripPhoto -> udumeoli.tripphoto (Kotlin 컨벤션)
    - detekt 1.23.7 -> 1.23.6 (Kotlin 1.9.x 호환)
    - detekt configuration의 Kotlin 버전을 1.9.23으로 강제 (런타임 충돌 회피)
    - detekt.yml 추가: SpreadOperator 비활성, EmptyFunctionBlock 테스트 제외

--- src/test/kotlin/udumeoli/tripphoto/TripPhotoApplicationTests.kt
+++ src/test/kotlin/udumeoli/tripphoto/TripPhotoApplicationTests.kt
@@ -1,13 +1,11 @@
-package udumeoli.TripPhoto
+package udumeoli.tripphoto
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class TripPhotoApplicationTests {
-
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
